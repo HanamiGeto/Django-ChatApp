8d2a723f770a9f9bfdded14a8a9798df9c00528a
HanamiGeto <tidu11@hotmail.de>
2023-06-27 13:35:28 +0200

    added send message function

--- chat/views.py
+++ chat/views.py
@@ -1,23 +1,42 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from .models import Chat, Message
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core import serializers
 
-# Create your views here.
+
+@login_required(login_url='/login/')
 def index(request):
     if request.method == 'POST':
         print('Received data ' + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        serialized_obj = serializers.serialize('json', [ new_message, ])
+        return JsonResponse(serialized_obj[1:-1], safe=False)
     chatMessages = Message.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
 def login_view(request):
+    redirect = request.GET.get('next')
     if request.method == 'POST':
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
         if user:
             login(request, user)
-            return HttpResponseRedirect('/chat/')
+            return HttpResponseRedirect(request.POST.get('redirect'))
         else:
-            return render(request, 'auth/login.html', {'wrongPassword': True})
-    return render(request, 'auth/login.html')
+            return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
+    return render(request, 'auth/login.html', {'redirect': redirect})
+
+def register_view(request):
+    redirect = request.GET.get('next')
+    if request.method == 'POST':
+        user = User.objects.create_user(username=request.POST.get('username'),
+                                        email=request.POST.get('email'),
+                                        password=request.POST.get('password'))
+        user.first_name = request.POST.get('username')
+        user.save()
+        if user:
+            return HttpResponseRedirect('auth/login.html')
+    return render(request, 'register/register.html', {'redirect': redirect})
